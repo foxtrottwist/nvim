126c25f82df6f16d83984f592a46ec8d4e0a191e
Law Horne <foxtrottwist@ymail.com>
2021-08-16 22:53:38 -0400

    bikeshedding

--- lua/options.lua
+++ lua/options.lua
@@ -67,3 +67,41 @@ opt.wrap = true -- Wrap lines
 -- believe the following should work, but it does not https://github.com/neovim/neovim/pull/13479#event-4813249467
 -- opt.whichwrap = opt.whichwrap + {"<", "" > "", "h", "l"}
 
+-- Runs Formatter on save
+vim.api.nvim_exec(
+	[[
+  augroup FormatAutogroup
+    autocmd!
+    autocmd BufWritePost *.js,*.ts,*.tsx,*.css,*.scss,*.md,*.html,*.lua : FormatWrite
+  augroup END
+  ]],
+	true
+)
+
+-- Visual mode pressing * or # searches for the current selection
+-- Super useful! From an idea by Michael Naumann
+vim.cmd [[
+  vnoremap <silent> * :<C-u>call VisualSelection('', '')<CR>/<C-R>=@/<CR><CR>
+  vnoremap <silent> # :<C-u>call VisualSelection('', '')<CR>?<C-R>=@/<CR><CR>
+
+  function! CmdLine(str)
+      all feedkeys(":" . a:str)
+  endfunction
+
+  function! VisualSelection(direction, extra_filter) range
+      let l:saved_reg = @"
+      execute "normal! vgvy"
+
+      let l:pattern = escape(@", "\\/.*'$^~[]")
+      let l:pattern = substitute(l:pattern, "\n$", "", "")
+
+      if a:direction == 'gv'
+          call CmdLine("Ack '" . l:pattern . "' " )
+      elseif a:direction == 'replace'
+          call CmdLine("%s" . '/'. l:pattern . '/')
+      endif
+
+      let @/ = l:pattern
+      let @" = l:saved_reg
+  endfunction
+]]
