058108883c3c487a8cd99ed3059c4c66433d897b
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2023-03-14 21:23:15 -0400

    Updated barbar options; removed unused README.

--- lua/foxtrottwist/plugins/barbar.lua
+++ lua/foxtrottwist/plugins/barbar.lua
@@ -4,5 +4,11 @@ if not status then
 end
 
 barbar.setup({
+	-- Enable/disable animations
+	animation = false,
+	-- Enable/disable auto-hiding the tab bar when there is a single buffer
+	auto_hide = true,
+	-- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
+	hide = { extensions = true, inactive = true },
 	letters = "arstneiogmkhxcdvzlywfpubjq;ARSTNEIGMKHXCDVZLYWFPUBJQ:",
 })
