dae50fa3696df9a341a22c6a9e95197890cabc29
Lawrence Horne <lawrencehorne@Citrus-maxima.local>
2023-05-14 12:02:11 -0400

    Fixed folder icons.

--- lua/foxtrottwist/plugins/nvim-tree.lua
+++ lua/foxtrottwist/plugins/nvim-tree.lua
@@ -8,25 +8,11 @@ end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
--- change color for arrows in tree to light blue
-vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-
 -- configure nvim-tree
 nvimtree.setup({
 	view = {
 		width = 50,
 		side = "right",
-	},
-	-- change folder arrow icons
-	renderer = {
-		icons = {
-			glyphs = {
-				folder = {
-					arrow_closed = "", -- arrow when folder is closed
-					arrow_open = "", -- arrow when folder is open
-				},
-			},
-		},
 	},
 	-- disable window_picker for
 	-- explorer to work well with
