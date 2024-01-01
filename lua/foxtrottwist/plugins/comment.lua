6f7c08ec023911442c6a11ee348e8056bb3fd397
Law Horne <hello@foxtrottwist.com>
2024-01-01 10:57:12 -0500

    Fixed comment config.

--- lua/foxtrottwist/plugins/comment.lua
+++ lua/foxtrottwist/plugins/comment.lua
@@ -1,9 +1,9 @@
 return {
 	"numToStr/Comment.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	-- dependencies = {
-	"JoosepAlviste/nvim-ts-context-commentstring",
-	-- },
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
 	config = function()
 		-- import comment plugin safely
 		local comment = require("Comment")
