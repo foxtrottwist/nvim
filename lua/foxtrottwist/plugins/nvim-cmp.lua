051ed6625df6b8c39ad63f3c6776e87f6d5fe53c
Law Horne <hello@foxtrottwist.com>
2024-01-01 19:59:39 -0500

    Added sourcegraph/sg.nvim; Cody.

--- lua/foxtrottwist/plugins/nvim-cmp.lua
+++ lua/foxtrottwist/plugins/nvim-cmp.lua
@@ -37,6 +37,7 @@ return {
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
+				{ name = "cody" }, -- sourcegraph cody
 				{ name = "nvim_lsp" }, -- lsp
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
