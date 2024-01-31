204a6116ddc333d7bed2c82a96c88302bdaf1af8
Law Horne <hello@foxtrottwist.com>
2024-01-31 14:14:38 -0500

    Added swift_format to format.lua

--- lua/foxtrottwist/plugins/format.lua
+++ lua/foxtrottwist/plugins/format.lua
@@ -18,6 +18,7 @@ return {
 				markdown = { "prettier" },
 				graphql = { "prettier" },
 				lua = { "stylua" },
+				swift = { "swift_format" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
